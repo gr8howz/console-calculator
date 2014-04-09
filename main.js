036fb15f7255517256fb1217039625e10a71dd1e
gr8howz <souledout81@yahoo.com>
2014-04-09 15:27:33 -0500

    added factorial and add

--- main.js
+++ main.js
@@ -1,3 +1,59 @@
+var whatOperation = "Press '+', '-', '*', '/', 'exp', 'sqrt', '!' to perform your calculation, '?' for help, or (q)uit";
+var operation = prompt(whatOperation);
+var num1, num2, result;
+var output;
+
+while(operation !== 'q') {
+
+  switch(operation){
+  case '+':
+    console.log('you hit +');
+    num1 = prompt("Enter number");
+    output = "The sum of " + num1 + " and " + num2 + " = " + add(num1,num2);
+    break;
+
+  case '-':
+    console.log('you hit -');
+    break;
+
+  case '*':
+    console.log('you hit *');
+    break;
+
+  case '/':
+    console.log('you hit /');
+    break;
+
+  case 'exp':
+    console.log('you hit exp');
+    break;
+
+  case 'sqrt':
+    console.log('you hit sqrt');
+    num1 = prompt("Enter square root number");
+    output = "The sqrt of " + num1 + " is " + sqrt(num1);
+    break;
+
+  case '!':
+    console.log('you hit !');
+    num1 = prompt("Enter factorial number");
+    num1 = num1 * 1;
+    output = "The factorial of " + num1 + " is " + factorial(num1);
+    break;
+
+  case '?':
+    console.log('Google it!');
+    break;
+
+  default:
+    console.log("huh?");
+  }
+
+  console.log(output);
+  operation = prompt(whatOperation);
+}
+
+
 function add(x,y){
   var result = x + y;
   return result;
@@ -34,4 +90,5 @@ function factorial(n){
       result = result * (n-1);
       n = (n-1);
     }
+    return result;
 }
